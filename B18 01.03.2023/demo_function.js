e117bcbe6a5ffc17b1c39748ef6ddd539300e550
Ngoc Diem <le.quynh.ngoc.diem@gmail.com>
2023-03-01 22:38:59 +0700

    lesson 18 - demo functice

---
+++ B18 01.03.2023/demo_function.js
@@ -0,0 +1,23 @@
+// Nhập a, b. Tính tổng hai số đó
+// Nhập c, d. Tính tổng hai số đó
+
+// let a = parseInt(prompt("Please input a: "));
+// let b = parseInt(prompt("Please input b: "));
+// let sum AB = a +b;
+// console.log(sumAB);
+
+// let d = parseInt(prompt("Please input d: "));
+// let c = parseInt(prompt("Please input c: "));
+// let sumCD = c + d;
+// console.log(sumCD);
+
+function calulateSum() {
+    let n1 = parseInt(prompt("Please input n1: "));
+    let n2 = parseInt(prompt("Please input n2: "));
+    let total = n1 + n2;
+    console.log(total);
+}
+
+calulateSum(); //Nhập được lần 1
+calulateSum(); //Nhập được lần 2
+calulateSum(); //Nhập được lần 3
